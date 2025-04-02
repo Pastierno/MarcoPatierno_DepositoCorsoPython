fe104582e713623513513627a89922f50abe6405
Pastierno <marcopatierno.m@gmail.com>
2025-04-02 12:03:03 +0200

    Create 2 - Decoratori.py

---
+++ CorsoPython/4 - Mercoledì 02-04/2 - Decoratori.py
@@ -0,0 +1,30 @@
+def decorator(func):
+    def wrapper():
+        print('Prima della funzione')
+        func()
+        print('Dopo la funzione')
+    return wrapper
+    
+@decorator
+def saluta():
+    print('Ciao')
+    
+
+saluta()
+
+# Decoratore con parametri
+def decorator_arg(funzione):
+    def wrapper(*args, **kwargs):
+        print('Before')
+        result = funzione(*args, **kwargs)
+        print('After')
+        return result + 2
+    return wrapper
+
+@decorator_arg
+def somma(a, b):
+    return a+b
+print(somma(3,4))
+
+
+    
