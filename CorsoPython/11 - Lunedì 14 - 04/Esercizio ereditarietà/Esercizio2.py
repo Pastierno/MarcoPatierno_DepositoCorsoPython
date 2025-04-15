663a43ccf7dcd9985a645448628626ce2fdc0027
Pastierno <marcopatierno.m@gmail.com>
2025-04-15 09:03:17 +0200

    Update Esercizio2.py

--- CorsoPython/11 - Lunedì 14 - 04/Esercizio ereditarietà/Esercizio2.py
+++ CorsoPython/11 - Lunedì 14 - 04/Esercizio ereditarietà/Esercizio2.py
@@ -170,8 +170,8 @@ def partita():
         punteggio_totale1 = sum([giocatore[6] for giocatore in squadra1 if giocatore[6] is not None]) 
         punteggio_totale2 =  sum([giocatore[6] for giocatore in squadra2 if giocatore[6] is not None])
     
-    print(f"Punteggio totale di della prima squadra: {punteggio_totale1}")
-    print(f"Punteggio totale di della seconda squadra: {punteggio_totale2}")
+    print(f"Punteggio totale della prima squadra: {punteggio_totale1}")
+    print(f"Punteggio totale della seconda squadra: {punteggio_totale2}")
     
     # Simulazione della partita
     tentativi_attacco = 15
